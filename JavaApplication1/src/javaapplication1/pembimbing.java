8eccc29ccbd14dec0d66ba71b302ae4f159666be
JOKER <JOKER@DESKTOP-Q6EOCHP>
2016-03-18 13:22:11 +0700

    pembimbing udah beres

---
+++ JavaApplication1/src/javaapplication1/pembimbing.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication1;
+
+/**
+ *
+ * @author JOKER
+ */
+public class pembimbing {
+    public long no_hpDosen;
+    public String ruangDosen;
+
+    public void setNo_hpDosen(long no_hpDosen) {
+        this.no_hpDosen = no_hpDosen;
+    }
+
+    public void setRuangDosen(String ruangDosen) {
+        this.ruangDosen = ruangDosen;
+    }
+    
+    
+}
