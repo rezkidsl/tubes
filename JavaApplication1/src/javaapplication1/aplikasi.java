1cdb782b243762d1a34e8dc3a38663767104d7ac
JOKER <JOKER@192.168.12.28>
2016-04-02 15:59:40 +0700

    aplikasi

--- JavaApplication1/src/javaapplication1/aplikasi.java
+++ JavaApplication1/src/javaapplication1/aplikasi.java
@@ -11,6 +11,73 @@ package javaapplication1;
  * @author JOKER
  */
 public class aplikasi {
-    private pembimbing daftarPembimbing[] = new pembimbing[2];
-    private mahasiswa 
+    public int bykpembimbing=0;
+    public int bykmhs=0;
+    public int byklokasi;
+    private pembimbing daftarPembimbing[] = new pembimbing[bykpembimbing];
+    private mahasiswa daftarMahasiswa[] = new mahasiswa[bykmhs];
+    private lokasi daftarLokasi[] = new lokasi[byklokasi];
+    
+    public void addPembimbing(pembimbing p){
+        daftarPembimbing[bykpembimbing] = p;
+        bykpembimbing++;
+    }
+    
+    public void addMahasiswa(mahasiswa m){
+        daftarMahasiswa[bykmhs] = m;
+        bykmhs++;
+    }
+    
+   public mahasiswa getMahasiswa(String nim){
+        for(int y=0; y < daftarMahasiswa.length ;y++){
+            if ( daftarMahasiswa[y].getId() == nim ){
+                return daftarMahasiswa[y];
+            }
+        }
+        return null;
+   }
+   public void deleteMahasiswa(String nim){
+          for(int i=0; i < daftarMahasiswa.length; i++){
+            if ( daftarMahasiswa[i].id == nim ){
+                for (int j=i; j < daftarMahasiswa.length; j++){
+                    daftarMahasiswa[j] = daftarMahasiswa[j+1];
+                }
+                bykmhs=bykmhs-1;
+            }
+        }
+   }
+   
+   public void registarasi(){
+       
+   }
+   public void loginmhs(){
+       
+   }
+   public void loginadmin(){
+       
+   }
+   public void lihatlokasi(){
+       
+   }
+   public void inputgeladi(){
+       
+   }
+   public void lihatgeladisaya(){
+       
+   }
+   public void kelolapembimbing(){
+       
+   }
+   public void addkelompok(){
+       
+   }
+   public void editkelompok(){
+       
+   }
+   public void logout(){
+       
+   }
+   public void mainmenu(){
+       
+   }
 }
